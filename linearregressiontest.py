6a151ce0d31b2577d46f835db2951d07a8d72e83
Jan Sowa <jan.piotr.sowa@gmail.com>
2023-03-25 08:21:30 +0100

    Move function to LinearRegression class, add 'fit' method

--- linearregressiontest.py
+++ linearregressiontest.py
@@ -8,14 +8,14 @@ class TestStringMethods(unittest.TestCase):
         """Calculate beta for function y = 1 + 2x_1"""
         X = np.array([[3], [2]])
         y = np.array([7, 5])
-        np.testing.assert_array_almost_equal(calculate_beta(X, y), [1, 2])
+        np.testing.assert_array_almost_equal(LinearRegression.calculate_beta(X, y), [1, 2])
 
     def test_calculate_beta2(self):
         """Calculate beta for function y = 1 + 2x_1,
         X provided as one-dimensional array"""
         X = np.array([3, 2])
         y = np.array([7, 5])
-        np.testing.assert_array_almost_equal(calculate_beta(X, y), [1, 2])
+        np.testing.assert_array_almost_equal(LinearRegression.calculate_beta(X, y), [1, 2])
 
     def test_calculate_beta3(self):
         """Calculate beta for function y = 1 + x_1 + 2x_2,
@@ -24,7 +24,7 @@ class TestStringMethods(unittest.TestCase):
                       [0, 2],
                       [2, 1]])
         y = np.array([4, 5, 5])
-        np.testing.assert_array_almost_equal(calculate_beta(X, y), [1, 1, 2])
+        np.testing.assert_array_almost_equal(LinearRegression.calculate_beta(X, y), [1, 1, 2])
 
 if __name__ == '__main__':
     unittest.main()
