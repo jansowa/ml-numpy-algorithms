02ba9a56aa2c933d7fff4865028b180949fee2ce
Jan Sowa <jan.piotr.sowa@gmail.com>
2023-03-26 14:23:53 +0200

    Implement methods and tests in LinearRegression: - calculate targets with given X and beta - fit and predict

--- linearregression.py
+++ linearregression.py
@@ -16,3 +16,14 @@ class LinearRegression:
 
     def fit(self, X: ArrayLike, y: ArrayLike):
         self.beta = self.calculate_beta(X, y)
+
+    def predict(self, X: ArrayLike):
+        if self.beta is None:
+            raise RuntimeError("The model is not fitted.")
+        return LinearRegression.calculate_targets(X, self.beta)
+
+    @staticmethod
+    def calculate_targets(X: ArrayLike, beta: ArrayLike):
+        X_temp = X if len(X.shape) > 1 else X.reshape((-1, 1))
+        X_ones = np.insert(X_temp, 0, 1, axis=1)
+        return np.matmul(X_ones, beta)
--- linearregressiontest.py
+++ linearregressiontest.py
@@ -2,7 +2,8 @@ import unittest
 import numpy as np
 from linearregression import *
 
-class TestStringMethods(unittest.TestCase):
+
+class TestLinearRegression(unittest.TestCase):
 
     def test_calculate_beta(self):
         """Calculate beta for function y = 1 + 2x_1"""
@@ -25,6 +26,42 @@ class TestStringMethods(unittest.TestCase):
                       [2, 1]])
         y = np.array([4, 5, 5])
         np.testing.assert_array_almost_equal(LinearRegression.calculate_beta(X, y), [1, 1, 2])
+
+    def test_calculate_targets(self):
+        """Calculate targets for given X and beta - function y = 1 + x_1 + 2x_2"""
+        X = np.array([[1, 1],
+                      [0, 2],
+                      [2, 1]])
+        beta = np.array([1, 1, 2])
+        np.testing.assert_array_almost_equal(LinearRegression.calculate_targets(X, beta), [4, 5, 5])
+
+    def test_calculate_targets2(self):
+        """Calculate targets for given X and beta - function y = 1 + x_1 + 2x_2"""
+        X = np.array([[1, 1],
+                      [0, 2],
+                      [2, 1],
+                      [5, 5]])
+        beta = np.array([1, 1, 2])
+        np.testing.assert_array_almost_equal(LinearRegression.calculate_targets(X, beta), [4, 5, 5, 16])
+
+    def test_fit_and_predict(self):
+        """Fit model and predict targets for function y = 1 + x_1 + 2x_2"""
+        X_train = np.array([[1, 1],
+                            [0, 2],
+                            [2, 1]])
+        y = np.array([4, 5, 5])
+
+        X_test = np.array([[2, 0],
+                           [3, 1],
+                           [4, 5],
+                           [10, 0]])
+        lr = LinearRegression()
+        lr.fit(X_train, y)
+
+        np.testing.assert_array_almost_equal(
+            lr.predict(X_test),
+            [3, 6, 15, 11])
+
 
 if __name__ == '__main__':
     unittest.main()
