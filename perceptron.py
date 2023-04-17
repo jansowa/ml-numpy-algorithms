35a6138c5b2d09d145df63897333b8ac9cfb8967
Jan Sowa <jan.piotr.sowa@gmail.com>
2023-04-17 18:25:04 +0200

    Implement rest of the perceptron algorithm

--- perceptron.py
+++ perceptron.py
@@ -1,13 +1,49 @@
 import numpy as np
 from numpy.typing import ArrayLike
 
+
 class Perceptron:
-    @staticmethod
-    def activation_function(X: ArrayLike, weights: ArrayLike) -> float:
-        X = np.array(X)
-        weights = np.array(weights)
-        return np.matmul(np.append(X, 1), weights.T)
+    _weights: ArrayLike = None
+
+    def fit(self, X, y, learning_rate: float = 0.1, epochs: int = 500):
+        self._weights = np.ones(X.shape[1] + 1)
+        for _ in range(epochs):
+            self._weights = Perceptron.single_epoch(X, self._weights, y, learning_rate=learning_rate)
+
+    def predict(self, X):
+        return Perceptron.make_prediction(X, self._weights)
 
     @staticmethod
-    def step_function(activation: float) -> int:
-        return 1 if activation >= 0 else 0
+    def activation_function(X: ArrayLike, weights: ArrayLike) -> ArrayLike:
+        X_ones = np.insert(X, 0, 1, axis=1)
+        weights = np.array(weights)
+        return np.matmul(X_ones, weights.T)
+
+    @staticmethod
+    def step_function(activation: ArrayLike) -> ArrayLike:
+        def single_step(single_activation: float):
+            return 1 if single_activation >= 0 else 0
+
+        return np.vectorize(single_step)(activation)
+
+    @staticmethod
+    def make_prediction(X: ArrayLike, weights: ArrayLike) -> ArrayLike:
+        return Perceptron.step_function(Perceptron.activation_function(X, weights))
+
+    @staticmethod
+    def single_epoch(X: ArrayLike, weights: ArrayLike, y: ArrayLike, learning_rate: float = 0.01):
+        y_pred = Perceptron.make_prediction(X, weights)
+        return weights + np.matmul(learning_rate * (y - y_pred), np.insert(X, 0, 1, axis=1))
+
+
+X = np.array([[1, 0, 1],
+              [1, 1, 0],
+              [0, 1, 1],
+              [0, 0, 0],
+              [1, 1, 1],
+              [0, 0, 1]])
+y = [0, 1, 1, 1, 1, 0]
+p = Perceptron()
+p.fit(X, y)
+print(p._weights)
+print(p.predict(X))
--- test/test_perceptron.py
+++ test/test_perceptron.py
@@ -4,15 +4,42 @@ from perceptron import *
 
 class TestPerceptron(unittest.TestCase):
     def test_activation_function(self):
-        X = [1, 2, 3, 4]
-        weights = [5, 4, 3, 2, 1]
-        self.assertAlmostEqual(Perceptron.activation_function(X, weights),
-                               31)
+        X = [[1, 0, 1],
+             [1, 1, 0],
+             [0, 1, 1],
+             [1, 1, 1]]
+        weights = [4, 3, 2, 1]
+        np.testing.assert_array_almost_equal(Perceptron.activation_function(X, weights),
+                                             [8, 9, 7, 10])
 
-    def step_function_test(self):
-        self.assertAlmostEqual(Perceptron.step_function(0.1),
-                               1)
-        self.assertAlmostEqual(Perceptron.step_function(0),
-                               1)
-        self.assertAlmostEqual(Perceptron.step_function(-0.1),
-                               0)
+    def test_step_function(self):
+        np.testing.assert_array_almost_equal(
+            Perceptron.step_function([0.1, -0.1, 100, -100]),
+            [1, 0, 1, 0]
+        )
+
+    def test_make_prediction(self):
+        X = [[-1, -1, 0],
+             [-1, -1, -1],
+             [1, 1, 1],
+             [0, 0, 0]]
+        weights = [4, 3, 2, 1]
+        np.testing.assert_array_almost_equal(
+            Perceptron.make_prediction(X, weights),
+            [0, 0, 1, 1]
+        )
+
+    def test_fit_and_predict(self):
+        X = np.array([[1, 0, 1],
+                      [1, 1, 0],
+                      [0, 1, 1],
+                      [0, 0, 0],
+                      [1, 1, 1],
+                      [0, 0, 1]])
+        y = [0, 1, 1, 1, 1, 0]
+        p = Perceptron()
+        p.fit(X, y)
+        np.testing.assert_array_almost_equal(
+            p.predict(X),
+            y
+        )
