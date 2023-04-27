c5a973b751bb5fadf0f2525493c121fa9239a91f
Jan Sowa <jan.piotr.sowa@gmail.com>
2023-04-27 14:22:16 +0200

    Add complete implementation of logistic regression calculated by gradient descent

--- logistic_regression_gradient_descent.py
+++ logistic_regression_gradient_descent.py
@@ -1,5 +1,6 @@
 import numpy as np
 from math import exp
+from numpy.typing import ArrayLike
 
 
 class LogisticRegressionGradientDescent:
@@ -11,18 +12,51 @@ class LogisticRegressionGradientDescent:
 
     @staticmethod
     def calculate_yhat_vector(beta: np.ndarray, X):
-        return 1 / (1 + np.exp(np.matmul(beta, X.T)))
+        return 1 / (1 + np.exp(-np.matmul(beta, X.T)))
 
     @staticmethod
-    def corrected_parameter(beta_j, alfa, y, y_hat, x_j):
-        return beta_j + alfa * ((y - y_hat) * x_j).sum()
+    def corrected_parameter(beta_j, lr, y, y_hat, x_j):
+        return beta_j + lr * ((y - y_hat) * x_j).sum()
+
+    def fit(self, X: ArrayLike, y: ArrayLike, max_epochs=10000, lr=0.01):
+        X_np = np.insert(np.array(X), 0, 1, axis=1)
+        self._beta = np.zeros(X_np.shape[1])
+        temp_beta = np.zeros(X_np.shape[1])
+        y_np = np.array(y)
+        y_hat = LogisticRegressionGradientDescent.calculate_yhat_vector(self._beta, X_np)
+        for _ in range(max_epochs):
+            for beta_idx in range(self._beta.shape[0]):
+                temp_beta[beta_idx] =\
+                    LogisticRegressionGradientDescent.corrected_parameter(
+                        temp_beta[beta_idx], lr, y_np, y_hat, X_np[:, beta_idx]
+                    )
+
+            self._beta = temp_beta
+            y_hat = LogisticRegressionGradientDescent.calculate_yhat_vector(self._beta, X_np)
+
+    def predict(self, X):
+        X_np = np.insert(np.array(X), 0, 1, axis=1)
+        return LogisticRegressionGradientDescent.calculate_yhat_vector(self._beta, X_np)
 
 
+X = np.array([[3, 4],
+              [2, 2],
+              [-1, -1],
+              [-2, -2],
+              [-2, -3],
+              [3, 3]])
+y = np.array([1, 1, 0, 0, 0, 1])
 
-beta = np.array([1, 1, 1, 1])
+model = LogisticRegressionGradientDescent()
+model.fit(X, y, max_epochs=1000)
+print(model.predict(X))
 
-X = np.array([[11, 10, 10, 10],
-              [0, 0, 0, 0],
-              [-10, -10, -10, -10]])
-
-print(LogisticRegressionGradientDescent.corrected_parameter(10, 0.01, np.array([1, 1, 1]), np.array([1, 0, 1]), 0.5))
+X_test = np.array([[1, 2],
+                   [2, 1],
+                   [3, 1],
+                   [1, 3],
+                   [-1, -2],
+                   [-2, -1],
+                   [-3, -1],
+                   [-1, -3]])
+print(model.predict(X_test))
