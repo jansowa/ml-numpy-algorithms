1942067e162a45a8a5d9c73850756ffa3eebb911
Jan Sowa <jan.piotr.sowa@gmail.com>
2023-06-02 11:23:06 +0200

    Extract theta_y to class field

--- naive_bayes_classifier.py
+++ naive_bayes_classifier.py
@@ -4,6 +4,8 @@ import numpy as np
 class NaiveBayesClassifier:
     __X: np.ndarray
     __y: np.ndarray
+    __theta_y1: float
+    __theta_y0: float
 
     @staticmethod
     def calculate_theta_y1(y: np.ndarray) -> float:
@@ -33,9 +35,7 @@ class NaiveBayesClassifier:
         return np.array([NaiveBayesClassifier.calculate_theta_j_y0(X, y, j) for j in np.where(sample == 0)[0]]).prod()
 
     @staticmethod
-    def calculate_class_1_probability(X: np.ndarray, y: np.ndarray, sample: np.ndarray) -> float:
-        theta_y1 = NaiveBayesClassifier.calculate_theta_y1(y)
-        theta_y0 = NaiveBayesClassifier.calculate_theta_y0(y)
+    def calculate_class_1_probability(X: np.ndarray, y: np.ndarray, sample: np.ndarray, theta_y1: float, theta_y0: float) -> float:
         product_theta_y1 = NaiveBayesClassifier.calculate_product_theta_y1(X, y, sample)
         product_theta_y0 = NaiveBayesClassifier.calculate_product_theta_y0(X, y, sample)
         nominator = product_theta_y1 * theta_y1
@@ -43,9 +43,7 @@ class NaiveBayesClassifier:
         return nominator / denominator
 
     @staticmethod
-    def calculate_class_0_probability(X: np.ndarray, y: np.ndarray, sample: np.ndarray) -> float:
-        theta_y1 = NaiveBayesClassifier.calculate_theta_y1(y)
-        theta_y0 = NaiveBayesClassifier.calculate_theta_y0(y)
+    def calculate_class_0_probability(X: np.ndarray, y: np.ndarray, sample: np.ndarray, theta_y1: float, theta_y0: float) -> float:
         product_theta_y1 = NaiveBayesClassifier.calculate_product_theta_y1(X, y, sample)
         product_theta_y0 = NaiveBayesClassifier.calculate_product_theta_y0(X, y, sample)
         nominator = product_theta_y0 * theta_y0
@@ -55,10 +53,17 @@ class NaiveBayesClassifier:
     def fit(self, X: np.ndarray, y: np.ndarray) -> None:
         self.__X = X
         self.__y = y
+        self.__theta_y0 = NaiveBayesClassifier.calculate_theta_y0(y)
+        self.__theta_y1 = NaiveBayesClassifier.calculate_theta_y1(y)
 
     def predict(self, sample: np.ndarray):
-        return np.argmax([NaiveBayesClassifier.calculate_class_0_probability(self.__X, self.__y, sample),
-                          NaiveBayesClassifier.calculate_class_1_probability(self.__X, self.__y, sample)])
+        class_0_prob = NaiveBayesClassifier.calculate_class_0_probability(self.__X, self.__y, sample, self.__theta_y1,
+                                                                         self.__theta_y0)
+        class_1_prob = NaiveBayesClassifier.calculate_class_1_probability(self.__X, self.__y, sample, self.__theta_y1,
+                                                                         self.__theta_y0)
+        print([class_0_prob, class_1_prob])
+        return np.argmax([class_0_prob,
+                          class_1_prob])
 
 y = np.array([1, 1, 0, 1, 1, 1])
 
@@ -70,10 +75,7 @@ X = np.array([[1, 1, 1, 1],
               [1, 0, 1, 1]])
 
 sample = np.array([0, 1, 1, 0])
-# print(NaiveBayesClassifier.calculate_class_1_probability(X, y, sample))
-# print(NaiveBayesClassifier.calculate_class_0_probability(X, y, sample))
 
 nbc = NaiveBayesClassifier()
 nbc.fit(X, y)
 print(nbc.predict(sample))
-# print(NaiveBayesClassifier.predict(X, y, sample))
