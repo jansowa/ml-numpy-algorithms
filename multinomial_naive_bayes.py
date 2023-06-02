3998439e1c1bf38dcc717416ccb91b465d05dd20
Jan Sowa <jan.piotr.sowa@gmail.com>
2023-06-02 17:37:58 +0200

    Calculate all multinomial naive bayes parameters

--- multinomial_naive_bayes.py
+++ multinomial_naive_bayes.py
@@ -4,8 +4,9 @@ import numpy as np
 class MultinomialNaiveBayes:
     __theta_y1: float
     __theta_y0: float
-    __theta_j_y1: np.ndarray
-    __theta_j_y0: np.ndarray
+    __theta_j_y1: dict
+    __theta_j_y0: dict
+    __vocab: np.ndarray
 
     @staticmethod
     def calculate_theta_y1(y: np.ndarray) -> float:
@@ -17,31 +18,49 @@ class MultinomialNaiveBayes:
         return (leny - y.sum()) / leny
 
     @staticmethod
-    def calculate_theta_j_y0(X: np.ndarray, y: np.ndarray) -> np.ndarray:
-        pass
+    def calculate_theta_j_y0(X_class_0: np.ndarray, vocab: np.ndarray) -> dict:
+        flatten = np.concatenate(X_class_0)
+        denominator = len(flatten)
+        nominator = np.array([len(flatten[flatten == word]) for word in vocab])
+        values = nominator / denominator
+        return dict(zip(vocab, values))
 
     @staticmethod
-    def calculate_theta_j_y1(X: np.ndarray, y: np.ndarray) -> np.ndarray:
-        pass
+    def calculate_theta_j_y1(X_class_1: np.ndarray, vocab) -> dict:
+        flatten = np.concatenate(X_class_1)
+        denominator = len(flatten)
+        nominator = np.array([len(flatten[flatten == word]) for word in vocab])
+        values = nominator / denominator
+        return dict(zip(vocab, values))
 
     def fit(self, X: np.ndarray, y: np.ndarray) -> None:
         self.__theta_y0 = MultinomialNaiveBayes.calculate_theta_y0(y)
         self.__theta_y1 = MultinomialNaiveBayes.calculate_theta_y1(y)
-        self.__theta_j_y0 = MultinomialNaiveBayes.calculate_theta_j_y0(X, y)
-        self.__theta_j_y1 = MultinomialNaiveBayes.calculate_theta_j_y1(X, y)
+        self.__vocab = np.unique(np.concatenate(X))
+        self.__theta_j_y0 = MultinomialNaiveBayes.calculate_theta_j_y0(X[y==0], self.__vocab)
+        self.__theta_j_y1 = MultinomialNaiveBayes.calculate_theta_j_y1(X[y==1], self.__vocab)
 
     def predict(self, sample: np.ndarray) -> int:
         pass
 
-X = np.array([np.array([0, 1, 0, 2]),
-              np.array([0, 3, 4, 5, 1, 2, 0]),
-              np.array([0, 6, 3, 6, 3, 2, 1, 6, 3, 2, 6, 1]),
-              np.array([1, 0, 2]),
-              np.array([0, 1, 2, 0, 2, 1]),
-              np.array([6, 4, 2, 6, 1, 7]),
-              np.array([8, 1, 2])],
+X = np.array([[0, 1, 0, 2],
+              [0, 3, 4, 5, 1, 2, 0],
+              [0, 6, 3, 6, 3, 2, 1, 6, 3, 2, 6, 1],
+              [1, 0, 2],
+              [0, 1, 2, 0, 2, 1],
+              [6, 4, 2, 6, 1, 7],
+              [8, 1, 2]],
              dtype=np.ndarray)
 
 y = np.array([0, 0, 1, 0, 1, 1, 1])
 
-print(np.array([np.array([1, 2, 3]), np.array([4, 5])], dtype=np.ndarray))
+flatten = np.concatenate(X[y == 0])
+flatten = np.concatenate(X)
+denominator = len(flatten)
+vocab = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+nominator = [len(flatten[flatten == word]) for word in vocab]
+values = np.array(nominator) / denominator
+print(nominator)
+print(denominator)
+print(values)
+print(dict(zip(vocab, values)))
