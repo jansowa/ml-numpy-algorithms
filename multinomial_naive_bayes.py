adcbb85d11bdf17373b7d0b7acb1f0aeac092a1f
Jan Sowa <jan.piotr.sowa@gmail.com>
2023-06-04 07:24:34 +0200

    Add laplace smoothing to multinomial naive bayes algorithm

--- multinomial_naive_bayes.py
+++ multinomial_naive_bayes.py
@@ -18,18 +18,18 @@ class MultinomialNaiveBayes:
         return (leny - y.sum()) / leny
 
     @staticmethod
-    def calculate_theta_j_y0(X_class_0: np.ndarray, vocab: np.ndarray) -> dict:
+    def calculate_theta_j_y0(X_class_0: np.ndarray, vocab: np.ndarray, laplace_smoothing: float) -> dict:
         flatten = np.concatenate(X_class_0)
-        denominator = len(flatten)
-        nominator = np.array([len(flatten[flatten == word]) for word in vocab])
+        denominator = len(flatten) + laplace_smoothing * len(vocab)
+        nominator = np.array([len(flatten[flatten == word]) + laplace_smoothing for word in vocab])
         values = nominator / denominator
         return dict(zip(vocab, values))
 
     @staticmethod
-    def calculate_theta_j_y1(X_class_1: np.ndarray, vocab: np.ndarray) -> dict:
+    def calculate_theta_j_y1(X_class_1: np.ndarray, vocab: np.ndarray, laplace_smoothing: float) -> dict:
         flatten = np.concatenate(X_class_1)
-        denominator = len(flatten)
-        nominator = np.array([len(flatten[flatten == word]) for word in vocab])
+        denominator = len(flatten) + laplace_smoothing * len(vocab)
+        nominator = np.array([len(flatten[flatten == word]) + laplace_smoothing for word in vocab])
         values = nominator / denominator
         return dict(zip(vocab, values))
 
@@ -59,12 +59,12 @@ class MultinomialNaiveBayes:
         denominator = product_theta_y0 * theta_y0 + product_theta_y1 * theta_y1
         return nominator / denominator
 
-    def fit(self, X: np.ndarray, y: np.ndarray) -> None:
+    def fit(self, X: np.ndarray, y: np.ndarray, laplace_smoothing: float = 0) -> None:
         self.__theta_y0 = MultinomialNaiveBayes.calculate_theta_y0(y)
         self.__theta_y1 = MultinomialNaiveBayes.calculate_theta_y1(y)
         self.__vocab = np.unique(np.concatenate(X))
-        self.__theta_j_y0 = MultinomialNaiveBayes.calculate_theta_j_y0(X[y == 0], self.__vocab)
-        self.__theta_j_y1 = MultinomialNaiveBayes.calculate_theta_j_y1(X[y == 1], self.__vocab)
+        self.__theta_j_y0 = MultinomialNaiveBayes.calculate_theta_j_y0(X[y == 0], self.__vocab, laplace_smoothing)
+        self.__theta_j_y1 = MultinomialNaiveBayes.calculate_theta_j_y1(X[y == 1], self.__vocab, laplace_smoothing)
 
     def predict(self, sample: np.ndarray):
         class_0_prob = MultinomialNaiveBayes.calculate_class_0_probability(sample, self.__theta_y1,
@@ -78,17 +78,17 @@ class MultinomialNaiveBayes:
                           class_1_prob])
 
 
-X = np.array([[0, 1, 0, 2, 3, 4, 5, 6, 7, 8],
-              [0, 3, 4, 5, 1, 2, 0],
-              [0, 6, 3, 6, 3, 2, 1, 6, 3, 2, 6, 1, 4, 5, 6, 7, 8],
-              [1, 0, 2],
-              [0, 1, 2, 0, 2, 1],
-              [6, 4, 2, 6, 1, 7],
-              [8, 1, 2]],
-             dtype=np.ndarray)
+X = np.array([[21, 23, 25, 27, 29],
+              [22, 24, 26, 28, 30],
+              [0, 2, 4, 6, 8, 10],
+              [1, 3, 5, 7, 9],
+              [21, 11, 22, 12, 23, 13, 24, 14],
+              [1, 11, 2, 12, 3, 13, 4, 14],
+              [15, 16, 17, 18, 19, 20, 8, 9, 10],
+              [15, 16, 17, 18, 19, 20, 21, 22, 23]], dtype=np.ndarray)
 
-y = np.array([0, 0, 1, 0, 1, 1, 1])
+y = np.array([0, 0, 1, 1, 0, 1, 1, 0])
 
 classifier = MultinomialNaiveBayes()
-classifier.fit(X, y)
-print(classifier.predict(np.array([0, 1, 2, 3, 4, 5, 6, 7])))
+classifier.fit(X, y, laplace_smoothing=1)
+print(classifier.predict(np.array([0, 1, 2, 3, 4, 21, 22, 23, 24, 25])))
