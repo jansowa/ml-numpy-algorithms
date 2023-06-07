cb8299473c0e8f2d4c8409fd2ac2f6db7c4dfc71
Jan Sowa <jan.piotr.sowa@gmail.com>
2023-06-07 21:14:44 +0200

    Simplify code of weighted linear regression calculated by normal equation

--- weighted_linear_regression_normal_equation.py
+++ weighted_linear_regression_normal_equation.py
@@ -3,26 +3,16 @@ from numpy.typing import ArrayLike
 
 
 class WeightedLinearRegressionNormalEquation:
-    _beta = None
-    _weights = None
+    _beta: np.ndarray
+    _weights: np.ndarray
 
     @staticmethod
-    def _calculate_beta(X: np.ndarray, y: np.ndarray, weights: np.ndarray) -> ArrayLike:
+    def _calculate_beta(X: np.ndarray, y: np.ndarray, weights: np.ndarray) -> np.ndarray:
         W = np.diag(weights)
         X_temp = X if len(X.shape) > 1 else X.reshape((-1, 1))
         X_ones = np.insert(X_temp, 0, 1, axis=1)
-        return np.matmul(
-            np.matmul(
-                np.matmul(
-                    np.linalg.inv(
-                        np.matmul(
-                            np.matmul(
-                                X_ones.T,
-                                W),
-                            X_ones)),
-                    X_ones.T),
-                W),
-            y)
+        return np.linalg.inv(X_ones.T.dot(W).dot(X_ones)) \
+            .dot(X_ones.T).dot(W).dot(y)
 
     def fit(self, X: np.ndarray, y: np.ndarray, weights: np.ndarray) -> None:
         self._beta = self._calculate_beta(X, y, weights)
@@ -36,4 +26,4 @@ class WeightedLinearRegressionNormalEquation:
     def _calculate_targets(X: np.ndarray, beta: np.ndarray) -> ArrayLike:
         X_temp = X if len(X.shape) > 1 else X.reshape((-1, 1))
         X_ones = np.insert(X_temp, 0, 1, axis=1)
-        return np.matmul(X_ones, beta)
+        return X_ones.dot(beta)
