ab6531bcbff0d92940ff47ed12550b9282fe9a27
Jan Sowa <jan.piotr.sowa@gmail.com>
2023-06-09 18:54:00 +0200

    First version of K Means Clustering

---
+++ k_means_clustering.py
@@ -0,0 +1,43 @@
+import numpy as np
+
+
+class KMeansClustering:
+    _means: np.ndarray
+
+
+    @staticmethod
+    def single_clustering_iteration(X: np.ndarray, old_means: np.ndarray) -> np.ndarray:
+        nearest = KMeansClustering.find_nearest_mean(X, old_means)
+        return np.array([np.mean(X[nearest == k], axis=0) for k in range(old_means.shape[0])])
+
+    @staticmethod
+    def find_nearest_mean_sample(X_sample: np.ndarray, means: np.ndarray):
+        return np.sqrt(np.square(X_sample - means).sum(axis=1)).argmin()
+
+    @staticmethod
+    def find_nearest_mean(X: np.ndarray, means: np.ndarray) -> np.ndarray:
+        return np.array([KMeansClustering.find_nearest_mean_sample(X_sample, means) for X_sample in X])
+
+    def fit(self, X: np.ndarray, k: int) -> None:
+        self._means = np.random.random_sample((k, X.shape[1]))
+        for _ in range(100): # TODO: set it right
+            self._means = KMeansClustering.single_clustering_iteration(X, self._means)
+
+    def predict(self, X: np.ndarray) -> np.ndarray:
+        return KMeansClustering.find_nearest_mean(X, self._means)
+
+
+X = np.array([[6, 4],
+              [2, 2],
+              [3, 5],
+              [6, 4],
+              [2, 4],
+              [-6, -7],
+              [-4, -6],
+              [-7, -8],
+              [-10, -11]])
+
+kmc = KMeansClustering()
+kmc.fit(X, 2)
+print(kmc._means)
+print(kmc.predict(X))
